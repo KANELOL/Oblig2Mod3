656d802f111d43eebc9429379f64a2f0c1ffca9e
KANELOL <65165205+KANELOL@users.noreply.github.com>
2020-11-12 09:57:21 +0100

    Add files via upload

---
+++ SimpleChess/Rook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SimpleChess
+{
+    class Rook: Piece
+    {
+        //public Rook(string symbol) : base(symbol) => Symbol = "TRN";
+        public Rook() : base("TRN")
+        {
+        }
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            Console.WriteLine("Rook Move");
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+
+       
+    }
+}
